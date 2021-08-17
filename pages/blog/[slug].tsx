23761be349cd9bb89180db893ef760c61b35193b
dev-ardha <ardhaydhtm@gmail.com>
2021-08-18 01:35:36 +0700

    Make blog page static

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,8 @@
 import Layout from "../../components/Layout";
-import { getPostBySlug } from "../../lib/graphql/queries";
-import Image from'next/image'
+import { getPostBySlug, getPosts } from "../../lib/graphql/queries";
+import Image from'next/image';
+import ReactMarkdown from 'react-markdown';
+import style from '../../styles/content.module.css';
 
 export default function Blog({ post }) {
     return (
@@ -8,7 +10,7 @@ export default function Blog({ post }) {
             <div className="container py-10 px-5">
                 <header>
                     <div className="max-w-screen-md m-auto mb-10">
-                        <h1 className="text-6xl font-bold leading-snug tracking-tight">{ post.title }</h1>
+                        <h1 className="text-6xl font-bold leading-tight tracking-tight">{ post.title }</h1>
                         <p className="text-gray-400 text-xl mt-4">{ post.description }</p>
                         <div className="flex mt-10 items-center">
                             <div className="rounded-full w-10 h-10 overflow-hidden relative bg-whitesmoke">
@@ -20,16 +22,32 @@ export default function Blog({ post }) {
                             </div>
                         </div>
                     </div>
-                    <div className="rounded overflow-hidden w-full h-144 relative">
-                        <Image objectFit="cover" sizes="100%" layout="fill" placeholder="blur" blurDataURL={ post.image.url } src={ post.image.url }/>
+                    <div className="mb-10">
+                        <div className="rounded overflow-hidden w-full h-144 relative mb-2">
+                            <Image objectFit="cover" sizes="100%" layout="fill" placeholder="blur" blurDataURL={ post.image.url } src={ post.image.url }/>
+                        </div>
+                        { post.image.url && <span className="text-sm">{ post.image.caption }</span> }
                     </div>
                 </header>
+                <div className="max-w-screen-md m-auto">
+                    <ReactMarkdown children={post.content} className={style.content}/>
+                </div>
             </div>
         </Layout>
     )
 }
 
-export async function getServerSideProps({ params: { slug } }) {
+export async function getStaticPaths() {
+    const { posts } = await getPosts();
+
+    const paths = posts.map((post) => ({
+        params: { slug: post.slug },
+    }))
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params: { slug } }) {
     const { posts } = await getPostBySlug(slug);
 
     return {
