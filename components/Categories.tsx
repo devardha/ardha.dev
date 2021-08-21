867501b6d60804fa749fd4e084f036bf13386b5b
dev-ardha <ardhaydhtm@gmail.com>
2021-08-22 02:53:21 +0700

    Fix missing key props

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -5,8 +5,8 @@ export default function Categories({ categories }){
         <div className="container">
             <ul className="px-5 mb-8 flex text-sm">
                 { categories.map((category, index) => (
-                    <Link href={'/blog/tag/' + category.slug}>
-                        <a><li className="px-3 py-2 bg-gray-100 hover:bg-gray-200 mr-3 cursor-pointer" key={ index }>{ category.name }</li></a>
+                    <Link href={'/blog/tag/' + category.slug} key={ index }>
+                        <a><li className="px-3 py-2 bg-gray-100 hover:bg-gray-200 mr-3 cursor-pointer">{ category.name }</li></a>
                     </Link>
                 )) }
             </ul>
