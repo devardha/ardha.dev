1b4fedff2079849109d3f78d51d3c1d63b492e2d
dev-ardha <ardhaydhtm@gmail.com>
2021-08-23 00:05:43 +0700

    Fix home mobile version

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Categories({ categories }){
     return(
         <div className="container">
-            <ul className="px-5 mb-8 flex text-sm">
+            <ul className="px-5 mb-8 flex text-sm flex-wrap md:flex-nowrap">
                 { categories.map((category, index) => (
                     <Link href={'/blog/tag/' + category.slug} key={ index }>
-                        <a><li className="px-3 py-2 bg-gray-100 hover:bg-gray-200 mr-3 cursor-pointer">{ category.name }</li></a>
+                        <a><li className="px-3 py-2 bg-gray-100 hover:bg-gray-200 mr-3 cursor-pointer mb-3 md:mb-0">{ category.name }</li></a>
                     </Link>
                 )) }
             </ul>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar(){
         <nav className="w-full fixed top-0 left-0 right-0 z-10 border-b border-gray-200" style={{ background: 'rgba(255,255,255,0.9)', backdropFilter: 'saturate(180%) blur(20px)' }}>
             <div className="container w-full">
                 <div className="px-5 py-6 w-full flex items-center">
-                    <ul className="flex flex-1 justify-start">
+                    <ul className="flex-1 justify-start hidden lg:flex">
                         { data.map((item, index) => (
                             <Link href={item.url} key={index}>
                                 <a><li className={`mx-4 text-sm hover:font-semibold ${ path === item.url ? 'font-semibold' : '' } ${ index === 0? 'ml-0' : '' }`}>{ item.title }</li></a>
